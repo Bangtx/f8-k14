b422acc000d1a5bbb2afd6fc44aa830cd1a0fe4c
Bangtx <banhtran.hha@gmail.com>
2025-05-16 07:44:16 +0700

    todo list

---
+++ js-core/2025-05-15/main.js
@@ -0,0 +1,15 @@
+const a = "hello;anh;em"
+
+// console.log(a.split(";"))
+
+const b = ["hello", "anh", "em"]
+// console.log(b.join("|"))
+
+
+
+
+let c = "test 1234"
+console.log(b.splice(1, 2))
+// for (let i = 0; i < c.length; i ++) {
+//   console.log(c[i])
+// }
