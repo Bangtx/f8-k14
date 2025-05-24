bc3d84880118305e66df239426c84757577e7dc6
Bangtx <banhtran.hha@gmail.com>
2025-05-25 06:48:50 +0700

    altp

---
+++ js-core/2025-05-24/oop.js
@@ -0,0 +1,7 @@
+import {Male} from './male.js'
+
+class BoyStudent extends Male {}
+
+const duc = new BoyStudent('duc', 22, 'quynh')
+
+duc.doDemo()
---
+++ js-core/2025-05-24/person.js
@@ -0,0 +1,30 @@
+export class Person {
+  #name;
+  #age;
+
+  constructor(name, age) {
+    this.#name = name
+    this.#age = age
+  }
+
+  setName(name) {
+    this.#name = name
+  }
+
+  setAge(age) {
+    this.#age = age
+  }
+
+  getName() {
+    return this.#name
+  }
+
+  getAge() {
+    return this.#age
+  }
+
+  eat() {
+    console.log('eat at person')
+  }
+
+}
