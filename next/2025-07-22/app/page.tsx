36b1e9d3f9fb8e7e121c8c604d3227fa908cb4d4
Bangtx <banhtran.hha@gmail.com>
2025-07-22 20:43:34 +0700

    get exams

--- next/2025-07-22/app/page.tsx
+++ next/2025-07-22/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// 'use client'
 
-export default function Home() {
+import axios from "axios";
+
+interface ProductI {
+  id: number
+  title: string
+}
+
+export default async function Home() {
+
+  const {data} = await axios.get('https://fakestoreapi.com/products')
+  const products: ProductI[] = data
+
   return (
-    <div className={styles.page}>
-    </div>
+    <>
+      <h1>Product List</h1>
+      <ul>
+        {
+          products.map(p => {
+            return <li key={p.id}>{p.title}</li>
+          })
+        }
+      </ul>
+    </>
   );
 }
