145b39f57ee672ee07c9eae3475457e994dec269
Bangtx <banhtran.hha@gmail.com>
2025-07-29 19:19:41 +0700

    get exams

---
+++ backend/2026-07-26/main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/test', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'test'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
