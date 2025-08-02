cf5bfd4d91fe3ecd953255d6162ea5d81f9712f4
Bangtx <banhtran.hha@gmail.com>
2025-08-02 19:35:41 +0700

    get exams

--- docker/db.sql
+++ docker/db.sql
@@ -52,10 +52,114 @@ update employee set active = false where id in (1, 3) and fullname = 'nguyen van
 update employee set active = false where (id = 1 or id = 3) and fullname = 'nguyen van a';
 
 
-insert into employee (id, fullname)
-values (1, 'nguyen van a'),
-       (2, 'nguyen van b'),
-       (3, 'nguyen van c');
+insert into employee (id, fullname, age)
+values (1, 'nguyen van a', 20),
+       (2, 'nguyen van b', 40),
+       (3, 'nguyen van c', 5000000000000000000);
+
+update employee set active = false where id = 2;
+
+SELECT * FROM employee
+WHERE fullname LIKE '%van c%';
+
+delete from employee;
+truncate employee;
+
+alter table employee add column new_age int8;
+update employee set new_age = age where age is not null;
+alter table employee drop column age;
+alter table employee rename new_age to age;
+
+
+drop table employee;
+create table if not exists employee (
+                                        id bigserial primary key,
+                                        name text,
+                                        age int8,
+                                        address text, -- default 'test'
+                                        active bool default true
+);
+
+insert into employee (id, name, age)
+values (1, 'nguyen van a', 20),
+       (2, 'nguyen van b', 40),
+       (3, 'nguyen van c', 60);
+
+insert into employee(name) values ('nguyen van d');
+insert into employee(name) values ('nguyen van e');
+
+select max(id) from employee;
+
+select setval('employee_id_seq', (select max(id) from employee));
+
+
+drop table employee;
+create table if not exists employee (
+                                        id bigserial primary key,
+                                        name text,
+                                        age int8,
+                                        address text, -- default 'test'
+                                        company_id bigint,
+                                        active bool default true
+);
+
+drop table company;
+create table if not exists company (
+                                       id bigserial primary key,
+                                       name text,
+                                       active bool default true
+);
+
+insert into employee (id, name, age, company_id)
+values (1, 'nguyen van a', 20, 1),
+       (2, 'nguyen van b', 40, 1),
+       (3, 'nguyen van c', 60, 3);
+
+insert into company (id, name)
+values (1, 'f8'),
+       (2, 'f9'),
+       (3, 'f10');
+
+select employee.id,
+       employee.name as name,
+       company.name as company
+from employee
+         left join company on company.id = employee.company_id
+where employee.name = 'nguyen van a';
+
+
+select company.id, company.name, employee.name
+from company
+         join employee on company.id = employee.company_id;
+
+select employee.id,
+       employee.name as name,
+       company.name as company
+from employee
+         right join company on company.id = employee.company_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
